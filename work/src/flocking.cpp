4444fd81eed80480845676c7a351c68f62e9641a
RubenMagallanes <rubenmmm3@gmail.com>
2016-05-26 17:29:11 +1200

    add functions for basic flock operations
    flock functions: init, add boid, update all
    boid functions: update, render

--- work/src/flocking.cpp
+++ work/src/flocking.cpp
@@ -1,10 +1,50 @@
 #include "cgra_math.hpp"
 #include "flocking.hpp"
 
-void update (boid *){
+/*  ~  FLOCK FUNCTIONS  ~  */
 
+void /* initialise flock $fl with $amt boids, each using $model as their geometry */
+init_flock (flock *fl, int amt, Geometry* model)
+{
+	int i; 
+	for (i= 0; i< amt; i++)
+	{
+		add_boid (fl, model);
+	}
 }
 
-void render (boid *){
-	
+void /* create and add a boid using model $m to flock $fl */
+add_boid (flock *fl, Geometry *m)
+{
+	boid b = boid ();
+	b.model = m;
+	b.position = vec2 (0, 0);		
+	b.direction = vec2 (0, 1);		//x,z coords
+	b.speed = 1;
+	fl->members.push_back (b);
+}
+
+void /* tells each boid in flock $fl to update. */
+update_all (flock *fl)
+{
+	int i;
+	for (i= 0; i< fl->members.size(); i++)
+	{
+		update(fl->members[i], fl);
+	}
+}
+
+/*  ~  BOID FUNCTIONS  ~  */
+
+void 
+update (boid *b, flock *fl)
+{
+	//
+}
+
+void 
+render (boid *b)
+{
+	//todo translate and rotate
+	b->model.renderGeometry();
 }
--- work/src/flocking.hpp
+++ work/src/flocking.hpp
@@ -1,4 +1,6 @@
-#include “cgra_math.hpp
+#pragma once
+#include "cgra_math.hpp"
+#include "geometry.hpp"
 
 struct boid
 {
@@ -6,12 +8,45 @@ struct boid
 	vec2 position;
 	vec2 direction;
 	float speed; 
+	//int id 	//maybe needed for equivence testing
 };
 
 struct flock {
-	
+	std::vector<boid> members; // 
 };
 
+/*     FLOCK FUNCTIONS     */
+
+/* 	initialise flock to a certain number of boids. each boid will 
+	use the supplied geometry as their model when asked to render. 
+*/
+void init_flock (flock *, int, Geometry *);
+
+/* 	create and add a boid to given flock.
+	boid will render using the supplied geo.
+	TODO: take x,z coords as arguments as position. 
+ */
+void add_boid (flock *, Geometry *);
+
+/*	updates behaviour of each boid in given flock.
+	TODO: for each boid, calculate which other boids should affect it,
+	construct an array containing these others and pass to update().
+ */
+void update_all (flock *);
+
+//void render_all (flock *); // TODO tell each boid to render itself
+
+/*     BOID FUNCTIONS     */
+
+/*	update specified boid's actions, influenced by each other boid
+	in specified flock. 
+	TODO: change argument to array of boids that should affect this,
+	let flock choose whih other boids to pass in.  
+
+*/
 void update (boid *, flock *);
 
+/*	render boid on the screen. 
+
+ */
 void render (boid *);
